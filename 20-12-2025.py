c50c5c5b575212382ffd3d138aa5df634a6184ba
gauravgaikwad1802-cmd <gauravgaikwad1802@gmail.com>
2025-12-20 11:21:51 +0530

    Add files via upload

---
+++ 20-12-2025.py
@@ -0,0 +1,28 @@
+# 1)Take two inputs (string). Print their sum as numbers and concatenation as strings.
+#a=input("")
+#b=input("")
+#c=a+b
+#f=int(a) + int(b)
+#print(c,f)
+
+# 2)Take input x = "10.5" Convert it to int safely and print its type
+"""a="10.5"
+x =int(float(a))
+print(x)
+print(type(x))"""
+
+# 3) Input a number. Print:square,cube,square root,(Use only operators, no math library)
+
+"""a=int(input("Enter any number :"))
+x=(a**2)
+print("The Square of given number is :",x)
+y=(a**0.5)
+print(y)"""
+
+#3️⃣ First, Last, Middle character
+"""a=input()
+print(a[0])
+print(a[-1])
+if len(a)%2==1:
+    print(a[len(a)//2])"""
+#
