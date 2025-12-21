5e8d64f6e033101f14ac37d68a2552f4b1192673
gauravgaikwad1802-cmd <gauravgaikwad1802@gmail.com>
2025-12-21 22:43:36 +0530

    Add files via upload

---
+++ Armstrong_number.py
@@ -0,0 +1,14 @@
+a=int(input(""))
+temp=a
+b=len(str(a))
+s=0
+while temp > 0:
+    d=  temp%10
+    s=s+d**b
+    temp=temp//10
+if s == a:
+    print("Armstrong Number")
+else:
+    print("Not an Armstrong Number")
+
+
---
+++ largest_of_3_number.py
@@ -0,0 +1,12 @@
+a=input("")
+b=input("")
+c=input("")
+if a>b:
+    print("a is big number")
+elif b>c:
+    print("b is big number")
+elif c>a:
+    print("c is big number")
+
+else:
+    ("c is grater than a and b")
